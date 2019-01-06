f149aa3f9db779b5b66c2c84ee4369db80b229f9
Allen Garvey <allen.garvey@gmail.com>
2019-01-05 22:19:48 -0500

    Remove unused sockets/channels

--- lib/seren_web/endpoint.ex
+++ lib/seren_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule SerenWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :seren
 
-  socket "/socket", SerenWeb.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
