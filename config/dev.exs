4ec5bf5474e2dba5bdcf5c2bdff43fdbfa2d3073
Allen Garvey <allen.garvey@gmail.com>
2019-01-05 22:18:10 -0500

    Fix deprecated way of specifying database adapter in config file

--- config/dev.exs
+++ config/dev.exs
@@ -38,7 +38,6 @@ config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
 config :seren, Seren.Repo,
-  adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
   database: "seren_dev",
--- lib/seren/repo.ex
+++ lib/seren/repo.ex
@@ -1,5 +1,5 @@
 defmodule Seren.Repo do
-  use Ecto.Repo, otp_app: :seren
+  use Ecto.Repo, otp_app: :seren, adapter: Ecto.Adapters.Postgres
 
   @doc """
   Dynamically loads the repository url from the
